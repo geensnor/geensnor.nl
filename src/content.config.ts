3001f8249c02d747d4a6daec44ecf00f46f354e1
Joris <jorisreijbroek@gmail.com>
2025-04-25 12:44:55 +0000

    eindje verder

--- src/content.config.ts
+++ src/content.config.ts
@@ -7,8 +7,8 @@ const blog = defineCollection({
   // Type-check frontmatter using a schema
   schema: z.object({
     title: z.string(),
-    // Transform string to Date object
-    date: z.coerce.date().optional(),
+    author: z.string(),
+    date: z.date(),
   }),
 });
 
