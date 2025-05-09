2c83a3c914c85ed68ba7a51991f4e16026c44b80
Joris <jorisreijbroek@gmail.com>
2025-05-09 12:15:23 +0000

    max berichten, titels onder berichten

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,7 @@ import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
+	base: '',
 	site: 'https://example.com',
 	integrations: [mdx(), sitemap()],
 });
