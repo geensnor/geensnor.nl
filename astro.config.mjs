f1b6938918474d7a20045dc8a2c1d45b7ee6179a
Joris <jorisreijbroek@gmail.com>
2025-09-01 13:14:40 +0200

    favicon bij build

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,30 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
-import mdx from '@astrojs/mdx';
-import sitemap from '@astrojs/sitemap';
+import { defineConfig } from "astro/config";
+import mdx from "@astrojs/mdx";
+import sitemap from "@astrojs/sitemap";
+import { copyFileSync } from "fs";
+import { fileURLToPath } from "url";
+import path from "path";
 
 // https://astro.build/config
 export default defineConfig({
-	base: '',
-	site: 'https://example.com',
-	integrations: [mdx(), sitemap()],
+  base: "",
+  site: "https://www.geensnor.nl",
+  integrations: [
+    mdx(),
+    sitemap(),
+    {
+      name: "copy-favicon",
+      hooks: {
+        "astro:build:start": () => {
+          // Even De Snor op z'n plek zetten zodat hij als favicon gebruikt kan worden
+          const sourcePath = path.resolve(
+            "node_modules/@geensnor/astro-componenten/assets/de-snor.svg"
+          );
+          const destPath = path.resolve("public/favicon.svg");
+          copyFileSync(sourcePath, destPath);
+        },
+      },
+    },
+  ],
 });
