bd5b85ac10e6fbddc9f32b1d83dd1aa7bc7c89a2
Joris <jorisreijbroek@gmail.com>
2025-10-09 18:08:41 +0200

    strakker schema

--- src/content.config.ts
+++ src/content.config.ts
@@ -1,6 +1,26 @@
 import { glob } from "astro/loaders";
 import { defineCollection, z } from "astro:content";
 
+const CategorySchema = z.enum([
+  "Internet",
+  "Privacy",
+  "Geensnor",
+  "Politiek",
+  "Bitcoins",
+  "Vertier",
+  "Handig",
+  "Wielrennerij",
+  "Muziek",
+  "Tips",
+  "Spullen",
+  "Kunst",
+  "Whisky",
+  "Zeilen",
+  "Klushoekje",
+  "Privacy",
+  "Security",
+]);
+
 const blog = defineCollection({
   // Load Markdown and MDX files in the `src/content/blog/` directory.
   loader: glob({ base: "./src/content/blog", pattern: "**/*.{md,mdx}" }),
@@ -9,6 +29,7 @@ const blog = defineCollection({
     title: z.string(),
     author: z.string(),
     date: z.date(),
+    categories: z.array(CategorySchema).min(1),
   }),
 });
 
