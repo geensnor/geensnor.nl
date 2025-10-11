68da0a5cd2310b26ffa5a04422ea4c4356c9ec7a
Joris <jorisreijbroek@gmail.com>
2025-10-11 15:07:29 +0200

    eslint

---
+++ eslint.config.js
@@ -0,0 +1,49 @@
+import js from "@eslint/js";
+import globals from "globals";
+import tseslint from "typescript-eslint";
+import json from "@eslint/json";
+import markdown from "@eslint/markdown";
+import css from "@eslint/css";
+import { defineConfig } from "eslint/config";
+import astro from "eslint-plugin-astro";
+
+export default defineConfig([
+  {
+    files: ["**/*.{js,mjs,cjs,ts,mts,cts}"],
+    plugins: { js },
+    extends: ["js/recommended"],
+    languageOptions: { globals: globals.browser },
+    rules: {
+      "@typescript-eslint/no-explicit-any": "off",
+    },
+  },
+  tseslint.configs.recommended,
+  {
+    files: ["**/*.json"],
+    plugins: { json },
+    language: "json/json",
+    extends: ["json/recommended"],
+  },
+  {
+    files: ["**/*.md"],
+    plugins: { markdown },
+    language: "markdown/commonmark",
+    extends: ["markdown/recommended"],
+  },
+  {
+    files: ["**/*.css"],
+    plugins: { css },
+    language: "css/css",
+    extends: ["css/recommended"],
+  },
+  {
+    files: ["**/*.astro"],
+    plugins: {
+      astro,
+    },
+    extends: ["astro/recommended"],
+    rules: {
+      "@typescript-eslint/no-explicit-any": "off",
+    },
+  },
+]);
