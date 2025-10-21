e9f6ef1b660f5e3e474df65d0bd05cec3e5ac138
Joris <jorisreijbroek@gmail.com>
2025-10-21 18:53:55 +0200

    dingen

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = "geensnor";
+export const SITE_TITLE = "geensnor.nl";
 export const SITE_DESCRIPTION = "Dat zit wel snor!";
